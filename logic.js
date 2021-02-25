27830d08a4aeddeee6966eca94ded4db70864506
Freud Múnera <freud.munera@qcode.co>
2021-02-25 10:11:45 -0500

    Some validations

--- logic.js
+++ logic.js
@@ -1,91 +1,89 @@
-const form  = document.getElementById('frmFreeTrial');
+const form = document.getElementById("frmFreeTrial");
 
-const firtName = document.getElementById('firtName');
-const firtNameError = document.querySelector('#firtName + span.error');
+const firtName = document.getElementById("firtName");
+const firtNameError = document.querySelector("#firtName + span.error");
 
-const lastName = document.getElementById('lastName');
-const lastNameError = document.querySelector('#lastName + span.error');
+const lastName = document.getElementById("lastName");
+const lastNameError = document.querySelector("#lastName + span.error");
 
-const email = document.getElementById('email');
-const emailError = document.querySelector('#email + span.error');
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
 
-const password = document.getElementById('password');
-const passwordError = document.querySelector('#password + span.error');
+const password = document.getElementById("password");
+const passwordError = document.querySelector("#password + span.error");
 
-
-firtName.addEventListener('input', function (event) {
-
-    if (firtName.validity.valid) {
-        firtNameError.textContent = '';
-        firtNameError.className = 'error';
-    } else {
-      showError();
-    }
-  });
-
-  lastName.addEventListener('input', function (event) {
-
-    if (lastName.validity.valid) {
-        lastNameError.textContent = '';
-        lastNameError.className = 'error';
-    } else {
-      showError();
-    }
-  });
-
-  email.addEventListener('input', function (event) {
-
-    if (email.validity.valid) {
-      emailError.textContent = '';
-      emailError.className = 'error';
-    } else {
-      showError();
-    }
-  });
-
-
-  password.addEventListener('input', function (event) {
-
-  if (password.validity.valid) {
-    passwordError.textContent = '';
-    passwordError.className = 'error';
+firtName.addEventListener("input", function (event) {
+  if (firtName.validity.valid) {
+    firtNameError.textContent = "";
+    firtNameError.className = "error";
   } else {
     showError();
   }
 });
 
-form.addEventListener('submit', function (event) {
+lastName.addEventListener("input", function (event) {
+  if (lastName.validity.valid) {
+    lastNameError.textContent = "";
+    lastNameError.className = "error";
+  } else {
+    showError();
+  }
+});
 
-  if(!email.validity.valid) {
+email.addEventListener("input", function (event) {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+password.addEventListener("input", function (event) {
+  if (password.validity.valid) {
+    passwordError.textContent = "";
+    passwordError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  if (
+    !firtName.validity.valid ||
+    !lastName.validity.valid ||
+    !email.validity.valid ||
+    !password.validity.valid
+  ) {
     showError();
     event.preventDefault();
+    return false
   }
-
-  // Show message ok
+  alert("Info correct");
 });
 
 function showError() {
-  if(firtName.validity.valueMissing) {
-    firtNameError.textContent = 'First name cannot be empty';
-  } else if(firtName.validity.typeMismatch) {
-    firtNameError.textContent = 'First name cannot contain numbers';
+  if (firtName.validity.valueMissing) {
+    firtNameError.textContent = "First name cannot be empty";
+  } else if (firtName.validity.patternMismatch) {
+    firtNameError.textContent = "First name cannot contain numbers";
   }
 
-  if(lastName.validity.valueMissing) {
-    lastNameError.textContent = 'Last name cannot be empty';
-  } else if(lastName.validity.typeMismatch) {
-    lastNameError.textContent = 'Last name cannot contain numbers';
+  if (lastName.validity.valueMissing) {
+    lastNameError.textContent = "Last name cannot be empty";
+  } else if (lastName.validity.patternMismatch) {
+    lastNameError.textContent = "Last name cannot contain numbers";
   }
 
-  if(email.validity.valueMissing) {
-    emailError.textContent = 'Email cannot be empty';
-  } else if(email.validity.typeMismatch) {
-    emailError.textContent = 'Looks like this is not an email';
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Email cannot be empty";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Looks like this is not an email";
   }
 
-  if(password.validity.valueMissing) {
-    passwordError.textContent = 'Password cannot be empty';
-  } else if(password.validity.typeMismatch) {
-    passwordError.textContent = 'Looks like this is not an email';
+  if (password.validity.valueMissing) {
+    passwordError.textContent = "Password cannot be empty";
+  } else if (password.validity.typeMismatch) {
+    passwordError.textContent = "Looks like this is not an email";
   }
 }
